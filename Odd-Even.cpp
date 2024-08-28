ea2a308602f7ba43db800e8237b75da82aa75f32
MR:Baria.Akhilesh <bariaakhilesh65@gmail.com>
2024-08-28 15:51:18 +0100

    Add files via upload

---
+++ Odd-Even.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+   int a;
+   
+   cout << "Enter The Number to check.... Odd OR Even:" <<endl;
+   cin >> a;
+   
+   if(a%2==0)
+   {
+         cout << "number is Odd" << endl;
+   }
+   else
+   {
+       cout << "number is Even" << endl;
+   }
+   
+   
+   return 0;
+    }
